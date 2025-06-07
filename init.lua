84386f49e9f71411d1bc854e9fd955e67436b4c0
mihasket <miha.sket@gmail.com>
2025-06-08 00:28:41 +0200

    feat: file type plugin

--- init.lua
+++ init.lua
@@ -5,9 +5,9 @@ vim.keymap.set("n", "<space>x", "<cmd>:.lua<CR>")
 vim.keymap.set("n", "<space>tn", "<cmd>:Explore<CR>")
 
 vim.api.nvim_create_autocmd('TextYankPost', {
-	desc = 'Highlight when (yanking) copying text',
-	group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = 'Highlight when (yanking) copying text',
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
