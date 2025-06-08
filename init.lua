2428bb6686d1a3cefa01536a1158815634f56f93
mihasket <miha.sket@gmail.com>
2025-06-08 22:00:24 +0200

    feat: blink.cmp

--- init.lua
+++ init.lua
@@ -55,6 +55,3 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
-
-local stevilo = 5
-print(stevilo)
--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -2,18 +2,22 @@ return {
   {
     "neovim/nvim-lspconfig",
     dependencies = {
-      "folke/lazydev.nvim",
-      ft = "lua", -- only load on lua files
-      opts = {
-        library = {
-          -- See the configuration section for more details
-          -- Load luvit types when the `vim.uv` word is found
-          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      'saghen/blink.cmp',
+      {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+          library = {
+            -- See the configuration section for more details
+            -- Load luvit types when the `vim.uv` word is found
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
         },
-      },
+      }
     },
     config = function()
-      require('lspconfig').lua_ls.setup {}
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
+      require('lspconfig').lua_ls.setup { capabilities = capabilities }
 
       vim.api.nvim_create_autocmd('LspAttach', {
         callback = function(args)
