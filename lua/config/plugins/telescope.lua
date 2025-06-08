01b2235fe73ff50b218203208ed85414aab48e20
mihasket <miha.sket@gmail.com>
2025-06-08 16:32:08 +0200

    feat: telescope search help

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -7,13 +7,26 @@ return {
       { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
     },
     config = function()
+      require('telescope').setup {
+        pickers = {
+          find_files = {
+            theme = "ivy"
+          },
+          help_tags = {
+            theme = "ivy"
+          }
+        },
+        extensions = {
+          fzf = {}
+        }
+      }
+
+      vim.keymap.set("n", "<space>sh", require('telescope.builtin').help_tags)
       vim.keymap.set("n", "<space>sf", require('telescope.builtin').find_files)
       vim.keymap.set("n", "<space>en", function()
-        local opts = require('telescope.themes').get_ivy({
+        require('telescope.builtin').find_files {
           cwd = vim.fn.stdpath("config")
-        })
-
-        require('telescope.builtin').find_files(opts)
+        }
       end)
     end
   }
