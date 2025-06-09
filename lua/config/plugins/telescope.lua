037117cc3a1f2c33b2ebd25369a6aadf44a666f6
mihasket <miha.sket@gmail.com>
2025-06-09 16:27:43 +0200

    feat: additional keymaps

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -22,16 +22,24 @@ return {
       }
 
       require('telescope').load_extension('fzf')
+      local builtin = require 'telescope.builtin'
 
-      vim.keymap.set("n", "<space>sh", require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-      vim.keymap.set("n", "<space>sf", require('telescope.builtin').find_files, { desc = '[S]earch [F]ile' })
+      vim.keymap.set("n", "<space>sh", builtin.help_tags, { desc = '[S]earch [H]elp' })
+      vim.keymap.set("n", "<space>sf", builtin.find_files, { desc = '[S]earch [F]ile' })
       vim.keymap.set("n", "<space>en", function()
-        require('telescope.builtin').find_files {
+        builtin.find_files {
           cwd = vim.fn.stdpath("config")
         }
       end, { desc = '[E]dit [N]eovim config' })
 
       require "config.telescope.multigrep".setup()
+
+      vim.keymap.set('n', '<leader>/', function()
+        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+          winblend = 10,
+          previewer = false,
+        })
+      end, { desc = '[/] Fuzzily search in current buffer' })
     end
   }
 }
